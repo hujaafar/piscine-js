2e6a001d9776ad1c31fc8d5c6030b1b5df7bc238
hujaafar <hjawady703@gmail.com>
2024-08-28 02:44:53 +0300

    kk

---
+++ method-man.js
@@ -0,0 +1,17 @@
+function words(value){
+    return value.split("");
+}
+function sentence(value) {
+    return value.join("");
+}
+
+function yell(value) {
+    return value.toUpperCase;
+}
+function whisper(value) {
+    return value.toLowerCase("*");
+}
+function capitalize(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
+}
+
