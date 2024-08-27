342942cae6f073ed2b9df9d9914375c497bc4588
hujaafar <hjawady703@gmail.com>
2024-08-28 01:34:01 +0300

    kk

---
+++ last-first-kiss.js
@@ -0,0 +1,11 @@
+function first(value) {
+    return value[0];
+}
+
+function last(value) {
+    return value[value.length - 1];
+}
+
+function kiss(value) {
+    return value[0] + value[value.length - 1];
+}
