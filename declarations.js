1921cb8fc6974115f6a937de3b5eee17bc3e4989
hujaafar <hjawady703@gmail.com>
2024-08-27 17:21:49 +0300

    kkufhfi

--- declarations.js
+++ declarations.js
@@ -1,16 +1,21 @@
 const escapeStr ="`\\/\"'" ;
-const arr =Object.freeze([4, '2'])
-const obj = Object.freeze({
+const arr =[4, '2']
+const obj = {
     str:"Habib llolololo",
     num : 2,
     bool : true,
     undef: undefined  
-})
-const nested =Object.freeze({
-     arr :Object.freeze([4, '2']) ,
-     obj: Object.freeze({
+}
+const nested ={
+     arr: [4,undefined ,'2'] ,
+     obj: {
         str:"Habib llolololo",
         num : 2,
         bool : true,
-         })
-})
+         }
+}
+Object.freeze(obj)
+Object.freeze(nested)
+Object.freeze(arr)
+Object.freeze(nested.arr)
+Object.freeze(nested.obj)
