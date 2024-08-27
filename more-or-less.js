d101be299bda5a20ef220161e565edd24a2fe5ab
hujaafar <hjawady703@gmail.com>
2024-08-28 00:33:01 +0300

    kkufhfi

--- more-or-less.js
+++ more-or-less.js
@@ -1,14 +1,15 @@
 function more(num) {
-    return num++;
-} 
-function less  (num ) {
+    return ++num; 
+}
 
-    return num--;
-} 
-function add  (num1 , num2 ) {
+function less(num) {
+    return --num; 
+}
+
+function add(num1, num2) {
     return num1 + num2;
-} 
-function sub  (nunum1 , num2 ) {
-    return num1 - num2;
-} 
+}
 
+function sub(num1, num2) { 
+    return num1 - num2;
+}
