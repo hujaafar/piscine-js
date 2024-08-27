bb342e5072ecc561d1a5e43c538aec5d464c6a89
hujaafar <hjawady703@gmail.com>
2024-08-27 16:56:43 +0300

    lolo

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr ="`\\/\"'" ;
+const arr = [4, '2'];
+const obj = {
+    str:"Habib llolololo",
+    num : 2,
+    bool : true,
+    undef: undefined  
+}
+const nested = {
+    arr: [4, undefined, '2'],
+    obj: {
+        str: "Habib llolololo",
+        num: 2,
+        bool: true
+    }
+}
