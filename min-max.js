76dbf562f735fde853db2974582ac306880d9299
hujaafar <hjawady703@gmail.com>
2024-08-28 03:01:48 +0300

    kk

---
+++ min-max.js
@@ -0,0 +1,14 @@
+function max(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+function min(num1, num2) {
+    if (num1 < num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
