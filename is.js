bba32dbecc8e097c6800df267df83d77332f6563
hujaafar <hjawady703@gmail.com>
2024-08-28 03:40:51 +0300

    kk

--- is.js
+++ is.js
@@ -2,12 +2,14 @@ const is={}
 
     // Check if value is a number (and not NaN)
    is.num= function(value) {
-        return typeof value === 'number' && !isNaN(value)}
+        return typeof value === 'number'}
    
 
     // Check if value is NaN (Not a Number)
     is.nan= function(value) {
-        return typeof value === 'number' && isNaN(value)}
+    return Number.isNaN(value)
+}
+
    
 
     // Check if value is a string
@@ -16,42 +18,42 @@ const is={}
    
 
     // Check if value is a boolean
-   is. bool= function(value) {
+   is.bool= function(value) {
         return typeof value === 'boolean'}
    
 
     // Check if value is undefined
-   is. undef= function(value) {
-        return typeof value === 'undefined'}
+   is.undef= function(value) {
+        return  value === 'undefined'}
    
 
     // Check if value is defined (not undefined)
-   is. def= function(value) {
-        return typeof value !== 'undefined'}
+   is.def= function(value) {
+        return  value !== 'undefined'}
    
 
     // Check if value is an array
-   is. arr= function(value) {
+   is.arr= function(value) {
         return Array.isArray(value)}
    
 
     // Check if value is a simple object or null
-   is. obj= function(value) {
+   is.obj= function(value) {
         return value !== null && typeof value === 'object' && !Array.isArray(value)}
    
 
     // Check if value is a function
-  is.  fun= function(value) {
+  is.fun= function(value) {
         return typeof value === 'function'}
    
 
     // Check if value is truthy
-  is.  truthy= function(value) {
+  is.truthy= function(value) {
         return !!value}
    
 
     // Check if value is falsy
-   is. falsy= function(value) {
+   is.falsy= function(value) {
         return !value}
     
 
