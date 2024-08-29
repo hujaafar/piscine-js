004f9c32f50b41fac77a20cafb13b9d3e8b6a36e
hujaafar <hjawady703@gmail.com>
2024-08-29 20:35:13 +0300

    haib

---
+++ elementary.js
@@ -0,0 +1,20 @@
+function multiply(a,b){
+        for (let i = 0; i < b; i++) {
+            a+=b
+        }
+}
+
+function divide(a,b){
+for (let i = 0; i <b; i++) {
+    a-=b
+    
+}
+}
+
+function modulo(a,b){
+    for (let i = 0; i < b; i++) {
+        a-=a/b
+    }
+}
+
+
