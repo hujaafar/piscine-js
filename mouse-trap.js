47e700cc41ccdfe14439ef7272d1092458315622
HUSAIN JAAFAR <hujaafar@noreply.learn.reboot01.com>
2024-09-07 21:51:53 +0000

    s ssk

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,104 +1,70 @@
-import { places } from "./where-do-we-go.data.js";
-
-var scroll = window.scrollY;
-const location = document.createElement("a");
-location.classList.add("location");
-document.body.appendChild(location);
-document.addEventListener("DOMContentLoaded", () => {
-    selectPlace();
-});
-
-document.addEventListener("scroll", () => {
-    selectPlace();
-    scroll > window.scrollY
-        ? (document.querySelector(".direction").innerHTML = "N")
-        : (document.querySelector(".direction").innerHTML = "S");
-    scroll = window.scrollY;
-});
-
-const explore = () => {
-    places.sort(compareCoordinates);
-    console.log(places);
-    places.forEach(place => {
-        createSection(place);
-    });
-    const compass = document.createElement("div");
-    compass.classList.add("direction");
-    document.body.appendChild(compass);
-};
-
-
-const createSection = (place) => {
-    let section = document.createElement("section");
-    section.style.background = `url('./where-do-we-go_images/${
-        place.name.toLowerCase().replaceAll(/ /g, "-").split(",")[0]
-    }.jpg')`;
-    section.style.backgroundSize = "cover";
-    section.style.backgroundPosition = "center";
-    section.style.backgroundRepeat = "no-repeat";
-    section.style.width = "100%";
-    section.style.height = "100vh";
-    document.body.appendChild(section);
-};
-
-
-const selectPlace = () => {
-    const sectionHeight = window.innerHeight;
-    const scroll = window.scrollY + sectionHeight / 2;
-    const sectionIndex = Math.floor(scroll / sectionHeight);
-    const place = places[sectionIndex];
-
-    location.textContent = `${place.name}\n${place.coordinates}`;
-    location.href = `https://www.google.com/maps/place/${urlEncodeCoordinates(
-        place.coordinates
-    )}/`;
-    
-    console.log(
-        location.href
-            .split("%C2%B0").join("°")
-            .split("%22").join('"')
-            .split("%20").join(" ")
-    );
-    
-    location.target = "_blank";
-    location.style.color = place.color;
-};
-
-
-const urlEncodeCoordinates = (coordinates) =>
-    coordinates
-        .replaceAll(" ", "%20")
-        .replaceAll("°", "%C2%B0")
-        .replaceAll('"', "%22");
-
-
-const compareCoordinates = (a, b) => {
-    const parseCoordinates = (coord) => {
-        const direction = coord.split(" ")[0].split('"')[1];
-        const lat = coord.split(" ")[0];
-        let deg = parseInt(lat.split("°")[0]);
-        let min = parseInt(lat.split("°")[1].split("'")[0]);
-        let sec = parseInt(lat.split("°")[1].split("'")[1].split('"')[0]);
-
-        if (direction === "S") {
-            deg = -deg;
-            min = -min;
-            sec = -sec;
+let box
+let flag = true
+let x
+let y
+let circle
+export function createCircle() {
+    addEventListener("click", function () {
+        circle = document.createElement("div")
+        circle.className = "circle"
+        if (flag) {
+            circle.style.background = "white"
+            circle.style.left = x
+            circle.style.top = y
+        } else {
+            circle.style.background = 'var(--purple)'
+            circle.style.left = x
+            circle.style.top = y
         }
-        return { deg, min, sec };
-    };
-
-    const aParsed = parseCoordinates(a.coordinates);
-    const bParsed = parseCoordinates(b.coordinates);
-
-    return aParsed.deg !== bParsed.deg
-        ? bParsed.deg - aParsed.deg
-        : aParsed.min !== bParsed.min
-        ? bParsed.min - aParsed.min
-        : aParsed.sec !== bParsed.sec
-        ? bParsed.sec - aParsed.sec
-        : 0;
-};
-
-
-export { explore };
+        document.body.appendChild(circle)
+        flag = true
+    })
+}
+export function moveCircle() {
+    addEventListener("mousemove", e => {
+        document.querySelectorAll(".circleRem").forEach((elem) => {
+            elem.remove()
+        })
+        x = e.clientX - 25 + "px"
+        y = e.clientY - 25 + "px"
+        let circle = document.createElement("div")
+        circle.className = "circle"
+        circle.classList.add("circleRem")
+        if (flag) {
+            circle.style.background = "white"
+        } else {
+            circle.style.background = 'var(--purple)'
+        }
+        circle.style.left = e.clientX - 25 + "px"
+        circle.style.top = e.clientY - 25 + "px"
+        document.body.appendChild(circle)
+        if ((e.clientX >= box.getBoundingClientRect().left + 25 && e.clientX <= box.getBoundingClientRect().right - 25) && (e.clientY >= box.getBoundingClientRect().top + 25 && e.clientY <= box.getBoundingClientRect().bottom - 25)) {
+            document.querySelector(".circle").style.background = 'var(--purple)'
+            flag = false
+        }
+        if (!flag) {
+            if (e.clientX - 25 < box.getBoundingClientRect().left) {
+                circle.style.left = box.getBoundingClientRect().left + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientX + 25 > box.getBoundingClientRect().right) {
+                circle.style.left = box.getBoundingClientRect().right - 50 + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientY - 25 < box.getBoundingClientRect().top) {
+                circle.style.top = box.getBoundingClientRect().top + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientY + 25 > box.getBoundingClientRect().bottom) {
+                circle.style.top = box.getBoundingClientRect().bottom - 50 + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+        }
+    })
+}
+export function setBox() {
+    box = document.createElement("div")
+    box.className = "box"
+    document.body.appendChild(box)
+    console.log(box.getBoundingClientRect().bottom)
+}
